04b8539baabf070f4f5a491a778f655d55563fcc
lisi <lisi@git.com>
2018-12-06 23:43:59 +0800

    lisi

--- src/main/java/com/shengsiyuan/jdk8/stream/StreamTest1.java
+++ src/main/java/com/shengsiyuan/jdk8/stream/StreamTest1.java
@@ -1,7 +1,7 @@
 package com.shengsiyuan.jdk8.stream;
 
 import java.util.ArrayList;
-import java.util.Arrays;
+//import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -23,12 +23,7 @@ public class StreamTest1 {
 
 	    list.forEach(str -> System.out.println());
 
-//	    zhangsan
-//  		hello
-//  
-//
-//
-//
+
 //  world
     }
 }
